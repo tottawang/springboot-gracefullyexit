4a09e2008397aeb0a11a046e4c13ad89c969e0b0
Totta Wang <totta.wang@autodesk.com>
2018-04-15 22:52:57 +0800

    add SmartLifeCycle implementation

--- src/main/java/com/sample/servicename/resources/SampleComponent.java
+++ src/main/java/com/sample/servicename/resources/SampleComponent.java
@@ -2,10 +2,11 @@ package com.sample.servicename.resources;
 
 import javax.annotation.PreDestroy;
 
+import org.springframework.context.SmartLifecycle;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SampleComponent {
+public class SampleComponent implements SmartLifecycle {
 
   private String value;
 
@@ -22,6 +23,43 @@ public class SampleComponent {
   @PreDestroy
   public void destroy() {
     System.out.println("Im inside destroy...");
+  }
+
+  @Override
+  public void start() {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void stop() {
+    System.out.println("Im inside stop...");
+  }
+
+  @Override
+  public boolean isRunning() {
+    // stop won't be called if return false
+    return true;
+  }
+
+  @Override
+  public int getPhase() {
+    // Returning Integer.MAX_VALUE only suggests that
+    // we will be the first bean to shutdown and last bean to start
+    return 0;
+  }
+
+  @Override
+  public boolean isAutoStartup() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void stop(Runnable callback) {
+    System.out.println("Im inside stop with callback...");
+
+    // Shudown complete. Regular shutdown will continue.
+    callback.run();
   }
 
 }
